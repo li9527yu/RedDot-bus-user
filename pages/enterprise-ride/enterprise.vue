6cb4338fdd7fd3d931468a21317397c1cc7c2a00
lizhaoyu11 <2770404066@qq.com>
2020-12-20 09:39:40 +0800

    first commit

---
+++ pages/enterprise-ride/enterprise.vue
@@ -0,0 +1,15 @@
+<template>
+	<view>
+		enterprise
+	</view>
+</template>
+
+<script>
+	export default{
+		name: 'Enterprise',
+		
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/person-ride/person.vue
@@ -0,0 +1,92 @@
+<template>
+	<view class="order">
+		<form @submit="">
+			<view class="uni-list">
+				<view class="uni-list-cell">
+					<view class="uni-list-cell-db">
+						<picker mode="date" :value="date" :start="startDate" :end="endDate" @change="bindDateChange">
+							<view class="uni-input"><text>当前选择日期为：</text>{{date}}</view>
+						</picker>
+					</view>
+				</view>
+			</view>
+			<navigator  url="../show-map/ShowMap">
+				<input type="text" focus  placeholder="输入起点"  />
+			</navigator>
+			<navigator url="../show-map/ShowMap">
+				<input type="text" focus  placeholder="输入终点"  />
+			</navigator>			
+		<button form-type="submit">下单</button>
+			
+		</form>
+	</view>
+</template>
+
+<script>
+	export default{
+		name:'Person',
+		data() {
+		        const currentDate = this.getDate({
+		            format: true
+		        })
+		        return {
+		            date: currentDate
+		        }
+		    },
+		computed: {
+			// mapState(['forcedLogin', 'hasLogin', 'userName']),
+			startDate() {
+			    return this.getDate('start');
+			},
+			endDate() {
+			    return this.getDate('end');
+			}
+		},
+		methods:{
+			getDate(type) {
+				            const date = new Date();
+				            let year = date.getFullYear();
+				            let month = date.getMonth() + 1;
+				            let day = date.getDate();
+				
+				            if (type === 'start') {
+				                year = year;
+				            } else if (type === 'end') {
+				                year = year;
+				            }
+				            month = month > 9 ? month : '0' + month;;
+				            day = day > 9 ? day : '0' + day;
+				            return `${year}-${month}-${day}`;
+				        },
+			bindDateChange: function(e) {
+				this.date = e.target.value
+			},
+			ShowMap(){
+				this.$router.push('/page/show-map/ShowMap');
+			}
+		}
+	}
+</script>
+
+<style>
+	.hello {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+	}
+	
+	.title {
+		color: #8f8f94;
+		margin-top: 25px;
+	}
+	
+	.ul {
+		font-size: 15px;
+		color: #8f8f94;
+		margin-top: 25px;
+	}
+	
+	.ul>view {
+		line-height: 25px;
+	}
+</style>
